b6b7d2bb584eb13fdc0d538267cf5eca3ca2eaf1
Kyle Beckwith <78455503+zippyfish@users.noreply.github.com>
2022-02-04 10:19:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var level = "Level 1 (coal)"
 var cash = 0;
-var addthing = document.getElementById("addthing");
+var addthing = document.getElementById("plusamt");
 const element = document.getElementById("money");
-setInterval(function() {cash += 5; element.innerHTML = "$" + cash;}, 1050);
-setInterval(function() {addthing.innerHTML = "+5"}, 1050);
-
-
-setInterval(function() {addthing.innerHTML = "&nbsp;"}, 1030.9);
-document.getElementById("level").innerHTML = level;
+setInterval(function() { element.innerHTML = "$" + cash }, 99);
+setInterval(function(){cash += 5; new Audio("https://github.com/zippyfish/cat_miner/raw/main/Meow.ogg.mp3").play(); addthing.innerHTML = "+5";},1050);
+setInterval(function(){addthing.innerHTML = "&nbsp;"}, 1000)
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        cash += 10 ;
+        addthing.innerHTML = "+10";
+    }
+}
+function nothingToSeeHere() {
+    cash +=1000;
+    addthing.innerHTML = "nothing happened...";
+}
