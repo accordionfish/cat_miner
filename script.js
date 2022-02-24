ceb9259059b2a635c5ceb0dd98321d923584cff6
Kyle Beckwith <78455503+zippyfish@users.noreply.github.com>
2022-02-24 08:39:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+var level = 1;
 
 var cat = document.getElementById("cat")
 var cash = 0;
 var addthing = document.getElementById("plusamt");
 const element = document.getElementById("money");
-setInterval(function() { element.innerHTML = "$" + cash }, 99);
-setInterval(function(){cash += 5; new Audio("https://github.com/zippyfish/cat_miner/raw/main/Meow.ogg.mp3").play(); addthing.innerHTML = "+5";},1050);
-setInterval(function(){addthing.innerHTML = "&nbsp;"}, 1000)
+var lvl1interval = setInterval(function() { element.innerHTML = "$" + cash }, 99);
+var meow = setInterval(function(){cash += 5; new Audio("https://github.com/zippyfish/cat_miner/raw/main/Meow.ogg.mp3").play(); addthing.innerHTML = "+5";},1050);
+var lvl1addthing = setInterval(function(){addthing.innerHTML = "&nbsp;"}, 1000)
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
         cash += 10 ;
         addthing.innerHTML = "+10";
-        if (cash > 100) {
+        if (cash > 10) {
           cat.classList.add("rainbow");
           const myTimeout = setTimeout(stopRainbow , 5000);
+          checkLevel()
 
 } else {
   return true;
@@ -24,4 +26,24 @@ function nothingToSeeHere() {
 }
 function stopRainbow(){
   cat.classList.remove("rainbow")
+}
+function levelUp(){
+  level += 1;
+  if(level=2){
+  level2();
+  } else {
+   return true;
+  }
+}
+function checkLevel(){
+ if(cash>99 && cash < 200){
+ levelUp();
+ }else {
+ 
+ }
+}
+function level2(){
+ clearInterval(lvl1interval);
+ clearInterval(lvl1addthing);
+ setInterval(function(){element.innerHTML = "$" + cash; cash +=100; addthing.innerHTML = "+100"}, 1000)
 }
