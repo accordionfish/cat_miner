ded9424511af60bc6c580980e34202d60dd11d6e
Kyle Beckwith <78455503+zippyfish@users.noreply.github.com>
2022-05-15 16:08:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
+var ms1 = new Audio("./sounds/mine1.wav");
+var ms2 = new Audio("./sounds/mine2.wav");
 var plusamt = document.getElementById("plusamt");
 var elem = document.getElementById("cash");
 var cat = $("#cat");
 var pickaxe = document.getElementById("pickaxe");
 var aipickaxe = "notbought";
-var money = 0;
+window.money = 0;
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 var aipButton = document.getElementById("aip-btn");
 var zo = setInterval(function() {money += 5; plusamt.innerText = "+5";setTimeout(function(){plusamt.innerHTML = "&nbsp;";},800);}, 1000)
-var z = setInterval(function() {elem.innerHTML = "$" + numberWithCommas(money)}, 99);
+var z = setInterval(function() {elem.innerHTML = "<img src='./assets/catcoin1.png' height='15px'>" + numberWithCommas(money)}, 99);
 document.body.onkeyup = function(e) {
   if (e.keyCode == 32) {
       money += 10;
-        plusamt.innerHTML="+$10";
+        plusamt.innerHTML="+<img src='./assets/catcoin1.png' height='15px'>10";
         pickaxe.style.animation = 'none';
         pickaxe.offsetHeight; /* trigger reflow */
         pickaxe.style.animation = null; 
+        ms1.play();
   } else {
     return true;
   }
@@ -84,13 +87,13 @@ function IP_Interval(){
         pickaxe.style.animation = 'none';
         pickaxe.offsetHeight; /* trigger reflow */
         pickaxe.style.animation = null; 
+        ms2.play();
     } else {
       return true;
     }
   }
 }
 function checkForGoldPickaxe(){
-
   if(money > 1000000){
     money -= 1000000;
     pickaxe.src = "./assets/gold_pickaxe.png";
@@ -104,7 +107,47 @@ function checkForGoldPickaxe(){
 }
 function GP_Interval(){
   clearInterval(window.int1q); 
-  var g = setInterval(function() {money += 25000; plusamt.innerText = "+25,000";setTimeout(function(){plusamt.innerHTML = "&nbsp;";},800);}, 1000)
+  window.g = setInterval(function() {money += 25000; plusamt.innerText = "+25,000";setTimeout(function(){plusamt.innerHTML = "&nbsp;";},800);}, 1000)
+  document.body.onkeyup = function(e) {
+    if (e.keyCode == 32) {
+        money += 15000;
+        plusamt.innerHTML = "+15,000";
+        pickaxe.style.animation = 'none';
+        pickaxe.offsetHeight; /* trigger reflow */
+        pickaxe.style.animation = null; 
+        ms1.play();
+    } else {
+      return true;
+    }
+  }
+}
+function cfbgp(){
+  if(window.money > 5000000){
+    money -= 5000000;
+    pickaxe.src = "./assets/baguettepick.png";
+    pickaxe.style.animationDuration = "2.7s";
+    bgp();
+    document.getElementById("bgp-btn").disabled=true;
+    document.getElementById("bgp-btn").innerText="OWNED";
+  } else {
+    alert("You don't have enough money.");
+  }
+}
+function bgp(){
+  clearInterval(window.g); 
+  var g = setInterval(function() {money += 50000; plusamt.innerText = "+50k";setTimeout(function(){plusamt.innerHTML = "&nbsp;";},800);}, 1000)
+  document.body.onkeyup = function(e) {
+    if (e.keyCode == 32) {
+        money += 80000;
+        plusamt.innerHTML = "+80k";
+        pickaxe.style.animation = 'none';
+        pickaxe.offsetHeight; /* trigger reflow */
+        pickaxe.style.animation = null; 
+        ms1.play();
+    } else {
+      return true;
+    }
+  }
 }
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  alert("you are using mobile.  I plan to support mobile in the close future, but as of right now, the main game depends on the spacebar. ")
